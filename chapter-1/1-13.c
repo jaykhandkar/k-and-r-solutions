dc6d0508a7ca5d5807111b852eadecac994d6643
Jay Khandkar <jaykhandkar2002@gmail.com>
2021-03-04 11:25:25 +0530

    1-13.c - fixed buffer overflow issue

--- chapter-1/1-13.c
+++ chapter-1/1-13.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /*Exercise 1-13. Write a program to print a histogram of the length of words
  * in it's input. It is easy to draw the histogram with the bars horizontal, a
@@ -7,24 +8,40 @@
 
 #define WORD_MAX 50 /*arbitrary limit on word length*/
 
-int main()
+/*print the histogram horizontally or vertically, depending on whether the
+ * program is invoked as <argv[0] -h> or <argv[0] -v>*/
+int main(int argc, char *argv[])
 {
 	int nlength[WORD_MAX] = {0};
-	int len, state, c, max;
+	int len, state, c, max, vertical;
 	double q;
 
 	/*the histogram for the most frequently occuring word length shall be
 	 * this long, all other frequencies will be scaled accordingly*/
 	char *s = "----------------------------------------------------------";
 
+	if (argc != 2) {
+		printf("usage: 1-13 [-h][-v]\n");
+		exit(0);
+	}
+	if (strcmp(argv[1], "-h") == 0)
+		vertical = 0;
+	else if (strcmp(argv[1], "-v") == 0)
+		vertical = 1;
+	else{
+		printf("usage: 1-13 [-h][-v]\n");
+		exit(0);
+	}
+
 	len = state = max = 0;
 	while ((c = getchar()) != EOF) {
 		if ((c == ' ' || c == '\t')){
 			if (state) {
-				if (len < WORD_MAX)
+				if (len < WORD_MAX){
 					nlength[len]++;
-				if (nlength[len] > max)
-					max = nlength[len];
+					if (nlength[len] > max)
+						max = nlength[len];
+				}
 				len = 0;
 				state = 0;
 			}
@@ -34,6 +51,7 @@ int main()
 			state = 1;
 		len++;
 	}
+	printf("max=%d \n", max);
 	for (int i = 0; i < WORD_MAX; i++){
 		q = (double) nlength[i] / max;
 		if (nlength[i])
